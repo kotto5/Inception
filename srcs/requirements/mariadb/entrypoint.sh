046b2a653a914d6009a9aa8971942a15d99f6d4c
kotto5 <kotto555555@gmail.com>
2023-12-17 22:04:53 +0900

    Fix MariaDB initialization script
    commit body

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -2,20 +2,15 @@
 
 # if $DATADIR/mysql not exist, initialize database
 if [ ! -d "${MYSQL_DATA_PATH}/mysql" ]; then
-	# Don't know why, but it doesn't work
-    # mysql_install_db --datadir=${MYSQL_DATA_PATH}
 	rc-service mariadb setup
-	# Wait for mariadb to be ready
     rc-service mariadb start
-    mysqladmin -u root password "${MYSQL_ROOT_PASSWORD}"
-	echo "CREATE USER '${MYSQL_USER}' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> /tmp/sql
-    echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} DEFAULT CHARACTER SET utf8;"  >> /tmp/sql
-    echo "grant all privileges on ${MYSQL_DATABASE}.* to ${MYSQL_USER}@'%' identified by '${MYSQL_PASSWORD}' with grant option ;" >> /tmp/sql
-    echo "DELETE FROM mysql.user WHERE User='';" >> /tmp/sql
-    echo "DROP DATABASE test;" >> /tmp/sql
-    echo "FLUSH PRIVILEGES;" >> /tmp/sql
-    cat /tmp/sql | mysql -u root --password="${MYSQL_ROOT_PASSWORD}"
-    rm /tmp/sql
+
+	echo "CREATE USER '${DB_USER}' IDENTIFIED BY '${DB_PASS}';" >> sql_text
+    echo "CREATE DATABASE IF NOT EXISTS ${DB_NAME} DEFAULT CHARACTER SET utf8;"  >> sql_text
+    echo "grant all privileges on ${DB_NAME}.* to ${DB_USER}@'%' identified by '${DB_PASS}' with grant option ;" >> sql_text
+    cat sql_text | mysql -u root
+    # after this command, root password need to access as root user;
+    rm sql_text
     rc-service mariadb stop
 fi
 
