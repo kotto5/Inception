59c9a4092d68ea63a2390cf7693376496deed3bf
kotto5 <kotto555555@gmail.com>
2023-12-21 19:46:07 +0900

    WP_URL contain nginx's port. current problem is redirect default port (443)

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh +x
 
 wp core download --path=$WP_DATA_PATH --allow-root
+
+echo "define( 'WP_DEBUG', true );" >> /tmp/.testphp && \
+echo "define( 'WP_DEBUG_LOG', true );" >> /tmp/.testphp && \
 wp config create \
 	--path=$WP_DATA_PATH \
 	--dbname=$DB_NAME \
 	--dbuser=$DB_USER \
 	--dbpass=$DB_PASS \
-	--dbhost=$DB_HOST
+	--dbhost=$DB_HOST \
+	--extra-php < /tmp/.testphp && \
+rm /tmp/.testphp
+
 wp core install \
 	--path=$WP_DATA_PATH \
 	--url=$WP_URL \
@@ -26,3 +32,4 @@ exec php-fpm81 --nodaemonize
 
 # wp core install --path=$WP_DATA_PATH --url=$WP_URL --title="$WP_TITLE" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --locale="en_JP" --skip-email
 # wp user create $WP_EDITOR_USER $WP_EDITOR_EMAIL --path=$WP_DATA_PATH --role=author --user_pass=$WP_EDITOR_PASSWORD
+# wp option update siteurl $WP_URL$WP_DATA_PATH
